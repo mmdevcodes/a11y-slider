5c037542da62a5317bd1f201a3dd6c9bc1e8d5b8
Maurice <mmahandev@users.noreply.github.com>
2020-02-09 21:33:22 -0600

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-099a0d3170f95806855e.js"
+    "url": "webpack-runtime-44f3590ad3bacee9a120.js"
   },
   {
     "url": "commons-58486c2ce8c9aff80941.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8171c831b63bbfafeaf238c885909204"
+    "revision": "66dc7baaa46ad8c71acf1f9fc7c8a405"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
