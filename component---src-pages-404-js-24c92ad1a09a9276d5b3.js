bbeeebbfd2965ba238b066dbd4f1ae7b20bf7fa0
Maurice <mmahandev@users.noreply.github.com>
2020-02-10 22:12:28 -0600

    Updates

---
+++ component---src-pages-404-js-24c92ad1a09a9276d5b3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,t,l){"use strict";l.r(t);l("q1tI");var u=l("Bl7J"),b=l("qKvR");t.default=function(){return Object(b.b)(u.a,null,Object(b.b)("h1",null,"NOT FOUND"),Object(b.b)("p",null,"Not a valid URL"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-24c92ad1a09a9276d5b3.js.map
--- sw.js
+++ sw.js
@@ -26,20 +26,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-47ca81debcbe615d1d7f.js"
+    "url": "webpack-runtime-743ff895e65643533cf8.js"
   },
   {
-    "url": "commons-58486c2ce8c9aff80941.js"
+    "url": "commons-d7ff6d79c4be29919d78.js"
   },
   {
-    "url": "app-7dfe41c377a90048490f.js"
+    "url": "app-4d6c61d5a2583f849afb.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-b1f6a3883e47831e3173.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3c72474c12e4895d720a2fe59dec91cb"
+    "revision": "2a3f273412c3e4d2a5a0f67abfab996d"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -71,7 +71,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-7dfe41c377a90048490f.js`))) {
+  if (!resources || !(await caches.match(`/app-4d6c61d5a2583f849afb.js`))) {
     return await fetch(event.request)
   }
 
